0078424431ab35327a7342f529726ced0a56aac0
Diptanshu <diptanshu2002@outlook.com>
2022-04-01 13:49:26 +0530

    added signup hook and created fireStore connection

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import { useState, useRef } from 'react';
+
+
+import { collection, addDoc } from 'firebase/firestore'
+import { database } from '../../firebase/config'
+
+//hooks
+import { useSignup } from '../../hooks/useSignup'
 
 export default function Signup() {
+  
+
+  //GETTING HOOKS METHODS
+  const { error, signup } = useSignup()
+
+  //GETTING FORM DATA
+  const name = useRef()
+  const contact = useRef()
+  const email = useRef();
+  const password = useRef()
+
+  //HANDLE FUNCTION
+  async function handleSubmit(e){
+    e.preventDefault();
+    
+    signup(email.current.value, password.current.value)
+
+    const collectionRef = collection(database, 'users')
+    const docRef = await addDoc(collectionRef,{email : email.current.value,
+    contact : contact.current.value,
+    name : name.current.value})
+
+    console.log(docRef.id)
+    
+    console.log(email.current.value, password.current.value, name.current.value, contact.current.value )
+  }
+
+
   return (
-    <div>Signup</div>
+    <div>
+      <h1>Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Email</span>
+          <input 
+            type="email" 
+            required 
+            ref={email}
+            />
+        </label>
+
+        <label>
+          <span>password</span>
+          <input 
+            type="password" 
+            required 
+            ref={password}/>
+        </label>
+
+        <label>
+          <span>Name</span>
+          <input 
+            type="text" 
+            required 
+            ref={name}/>
+        </label>
+
+        <label>
+          <span>Contact</span>
+          <input 
+            type="telephone" 
+            required 
+            ref={contact}/>
+        </label>
+
+        <button type='submit' > submit </button>
+      </form>
+
+      {error && <p>{error}</p>}
+    </div>
   )
 }
