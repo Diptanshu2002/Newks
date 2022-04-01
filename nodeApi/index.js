966f91eb0f9337ff510c43d9395cabc5ea52af2f
Diptanshu <diptanshu2002@outlook.com>
2022-04-01 21:28:21 +0530

    api for running the python script

---
+++ nodeApi/index.js
@@ -0,0 +1,44 @@
+const PORT = 8080
+const express = require("express");
+const cors = require('cors')
+const {spawn} = require('child_process');
+const helmet = require("helmet");
+
+
+const app = express();
+
+//middleware------------------
+app.use(helmet());
+app.use(express.json());
+app.use(
+    cors({
+      origin : '*',
+    }))
+
+
+//
+app.get('/',(req,res)=>{
+    res.send("home")
+})
+app.get('/api/search',(req, res)=>{
+    const query = req.query.sear;
+    var data2;
+    const python = spawn('python3', ['script1.py', query]);
+    python.stdout.on('data', function (data) {
+       console.log('Pipe data from python script ...');
+       data2 = data.toString();
+    });
+   // in close event we are sure that stream from child process is closed
+   python.on('close', (code) => {
+      console.log(`child process close all stdio with code ${code}`);
+      // send data to browser
+      res.send(data2)
+ });
+    // console.log(query)
+    // res.send(query)
+})
+
+
+app.listen(PORT, ()=>{
+    console.log('running at port 8080')
+})
---
+++ nodeApi/script1.py
@@ -0,0 +1,4 @@
+import sys
+print('hello from python')
+variable = sys.argv[1]
+print("First name: " , type(variable))
