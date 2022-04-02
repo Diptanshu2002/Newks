7f3e609bac19f682b144c6ab44a2c9b78e98a5b8
Saptarshi Bhattacharya <saptarshib98@gmail.com>
2022-04-02 14:44:21 +0530

    simplified the output

--- nodeApi/news.py
+++ nodeApi/news.py
@@ -5,7 +5,7 @@ newsapi = NewsApiClient(api_key="c3ecb2e0a22549f6bbdeedd6fa760ac6")
 
 # /v2/top-headlines
 query = input("enter company name: ")
-top_headlines = newsapi.get_everything(q=query)
+top_headlines = newsapi.get_top_headlines(q=query)
 
 
 # /v2/top-headlines/sources
